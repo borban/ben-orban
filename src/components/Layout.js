981d3097917ca875dc5efb18ee3d25d7080ffdd7
Ben Orban <benjamin.orban@gmail.com>
2020-01-06 09:53:58 -0600

    Dummy data

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,14 @@ import NavButton from "./NavButton";
 class Layout extends Component {
 
     render() {
+        const navHome = () => {
+            alert("Navigated Home!")
+        };
+
         return (
             <div>
                 <NavButton label="Home" navigate={navHome}/>
-                <NavButton label="Skills" navigate={nav}/>
+                <NavButton label="Skills" navigate={navHome}/>
                 <NavButton label="Experience" navigate={navHome}/>
                 <NavButton label="Accomplishments"navigate={navHome}/>
                 <NavButton label="GitHub" navigate={navHome}/>
