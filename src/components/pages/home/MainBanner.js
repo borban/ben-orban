1038d38f6c8b7954d2bb0779303ab512a87bf7c5
Ben Orban <benjamin.orban@gmail.com>
2020-01-06 20:15:43 -0600

    Adding MainBanner foundation

---
+++ src/components/pages/home/MainBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./MainBanner.css";
+
+const MainBanner = () => {
+    return (
+        <div className="MainBanner">
+
+        </div>
+    );
+};
+
+export default MainBanner;
