cbf22ccbb2202884d54453da1a3d2c00e1fd973a
Ben Orban <benjamin.orban@gmail.com>
2020-01-10 12:41:33 -0600

    Add quote

--- src/components/pages/home/MainBanner.js
+++ src/components/pages/home/MainBanner.js
@@ -5,7 +5,8 @@ const MainBanner = props => {
     return (
         <div className="MainBanner">
             <div className="MainBannerContent">
-                "As the transition to mobile devices has been rapidly occurring over the past five years, progressive EHR vendors have responded with clear plans and successful products to help expand the user experience. With more than half of physicians currently using a mobile device in their medical practices, these EHR vendors are allowing providers to choose which platform best fits in to their workflow in multiple healthcare delivery settings including highly usable mobile products."
+                “I have been impressed with the urgency of doing. Knowing is not enough; we must apply.
+                Being willing is not enough; we must do.” – Leonardo da Vinci
             </div>
         </div>
     );
